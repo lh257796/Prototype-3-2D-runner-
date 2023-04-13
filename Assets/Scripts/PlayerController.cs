4ed520dd677ecefb871ca40f139334021ca5b07b
Han <hl2341@nyu.edu>
2023-04-13 13:51:44 -0400

    playercontroller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float JumpForce = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //don't have access automatically to RigidBody, since not on every object
+        playerRb = GetComponent<Rigidbody>();
+        //<> gives you a type of something, ie to find a type of anything, like a collider
+        //need () to call it
+        //apply forces
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            //forceMode.impulse immediately applies force
+
+        }
+
     }
 }
